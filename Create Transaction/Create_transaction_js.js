9d17a9f51653e151f18242c94c2c170ae6f91dec
Viper43 <59294267+Viper43@users.noreply.github.com>
2020-06-28 21:28:27 +0530

    Delete Create_transaction_js.js

--- Create Transaction/Create_transaction_js.js
+++
@@ -1,9 +0,0 @@
-function enableReceiver() {
-    var t = document.getElementById("type");
-    if ( t.value == 'Money Transfer') {
-        document.getElementById("rec").style.display = 'block';
-    }
-    else {
-        document.getElementById("rec").style.display = 'none';
-    }
-}

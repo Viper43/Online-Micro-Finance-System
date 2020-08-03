e6d1cbe240371f0ff578c2103978f0b199c0ed17
Viper43 <rickk2014628@gmail.com>
2020-08-03 23:10:47 +0530

    delete

--- common variables/common_var.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $min_balance = 2000;                //Define minimum balance to be maintained for an account
-    $approval_percentage = 0.25;        //Define loan approval percentage
-    $savingsinterest_date = 27;         //Define savings interest date
-    $loanrepay_date = 5;                //Define auto loan repay date
-?>

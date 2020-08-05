2234056fe03bcb02fa3e71c6d86184b56e83ceb0
Viper43 <rickk2014628@gmail.com>
2020-08-05 19:50:21 +0530

    collector v1.3

--- collector/collector_back.php
+++ collector/collector_back.php
@@ -1,7 +1,7 @@
 <?php
 
     include "../connection.php";
-
+    echo "hello";
     $trno = $_POST["tno"];
     $accountno = $_POST["accountno"];
     $type = $_POST["type"];
@@ -22,7 +22,7 @@
             $query = $db->query("UPDATE transactions SET Status='Approved' WHERE Transaction_ID='{$trno}'");
             echo "<script type='text/javascript' >
             alert('Transaction Approved Successfully')
-            document.location='collector.php'
+            document.location='collector_main.php'
             </script>";
         }
         catch(PDOException $e)
